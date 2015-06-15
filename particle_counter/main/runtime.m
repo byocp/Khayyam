366e1c40d8a58671fd2c6965a51a395ca8a18d3d
dinospfc <i_am_cooler_than_kyle@hotmail.com>
2015-06-15 10:05:41 -0400

    Quick Fix
    Fixed some of the quick requested changes by Austria

--- particle_counter/main/runtime.m
+++ particle_counter/main/runtime.m
@@ -1,6 +1,6 @@
 global input_image_type particle_diameter_type parameter_over_time
 %'lucam_camera', 'video', 'single_image'
-input_image_type       = 'single_image';
+input_image_type       = 'lucam_camera';
 %'minor_axis', 'equiv_diam', 'major_axis'
 particle_diameter_type = 'major_axis'; 
 %'mean', 'contamination'
