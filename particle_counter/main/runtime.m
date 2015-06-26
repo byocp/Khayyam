903a37829ba3ce1d452cf7f7fedf1a641ea95122
RonanXin <rxin@kepstrum.com>
2015-06-26 11:39:30 -0400

    update Biye's software to work with Canada setup
    % 1. Save particle Histogram plot for each step to file Hist.jpeg in the
    root
    %       directry of software for further using in C# HMI display.
    % 2. Save particle g/L over time plot for each step to file g_per_L.jpeg
    in
    %       the root directry of the software for further using in C# HMI
    %       display.
    % 3. Re-design Histogram calculation function to frozen the bin size and
    range;
    %       re-place the old Histogram calculation function to speed up the
    %       process.
    % 4. Make particle Histogram calculation optional for 'minor_axis',
    %       'equiv_diam', 'major_axis'.
    % 5. Save particle Histogram calculation result in test data file
    %       contamination.txt in the root directry of software.

--- particle_counter/main/runtime.m
+++ particle_counter/main/runtime.m
@@ -1,8 +1,8 @@
 global input_image_type particle_diameter_type parameter_over_time
 %'lucam_camera', 'video', 'single_image'
-input_image_type       = 'single_image';
+input_image_type       = 'lucam_camera';
 %'minor_axis', 'equiv_diam', 'major_axis'
-particle_diameter_type = 'major_axis'; 
+particle_diameter_type = 'equiv_diam'; 
 %'mean', 'contamination'
 parameter_over_time    = 'contamination'; 
 hmi
